06989a2d11c2ad70cf8230d69c45ceae5d99fee8
al-shaimon <alshaimon968@gmail.com>
2022-11-15 02:08:21 +0600

    71-6 Responsive Grid for services section

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -6,7 +6,7 @@ const Banner = () => {
   return (
     <div className="hero">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <img src={chair} className="rounded-lg w-1/2 shadow-2xl" alt="banner" />
+        <img src={chair} className="rounded-lg lg:w-1/2 shadow-2xl" alt="banner" />
         <div>
           <h1 className="text-5xl font-bold">Box Office News!</h1>
           <p className="py-6">
