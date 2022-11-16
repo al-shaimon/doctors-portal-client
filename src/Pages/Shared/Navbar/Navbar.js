13015e213ecfaa770ae4a6522178dfdb6c69efee
al-shaimon <alshaimon968@gmail.com>
2022-11-16 20:09:30 +0600

    73-7 (Recap) Implement Login and auth state observer

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthProvider';
 
 const Navbar = () => {
+  const {user} = useContext(AuthContext)
   const menuItems = (
     <React.Fragment>
       <li>
@@ -19,9 +21,15 @@ const Navbar = () => {
       <li>
         <Link to="/contact">Contact Us</Link>
       </li>
-      <li>
-        <Link to="/login">Login</Link>
-      </li>
+      {user?.uid ?
+        <li>
+         <Link to="/login">Sign Out</Link>
+        </li>
+        :
+        <li>
+         <Link to="/login">Login</Link>
+        </li>
+      }
     </React.Fragment>
   );
 
