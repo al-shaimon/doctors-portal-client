4f66b75b2f76cc1261cea269f31ce0f0a1abc5af
al-shaimon <alshaimon968@gmail.com>
2022-11-16 19:34:32 +0600

    73-6 (Recap) Create Auth Context and implement user sign up

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider';
 
 const SignUp = () => {
   const {
@@ -9,8 +10,16 @@ const SignUp = () => {
     formState: { errors },
   } = useForm();
 
+  const { createUser } = useContext(AuthContext);
+
   const handleSignUp = (data) => {
     console.log(data);
+    createUser(data.email, data.password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => console.error(error));
   };
 
   return (
@@ -55,14 +64,14 @@ const SignUp = () => {
                 minLength: { value: 6, message: 'Password must be 6 characters long' },
                 pattern: {
                   value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/,
-                  message: 'Password must be strong',
+                  message: 'Password must have uppercase, number and special characters',
                 },
               })}
               className="input input-bordered w-full max-w-xs"
             />
             {errors.password && <p className="text-red-500">{errors.password.message}</p>}
           </div>
-          <input className="btn btn-accent w-full" value="Sign Up" type="submit" />
+          <input className="btn btn-accent w-full mt-4" value="Sign Up" type="submit" />
         </form>
         <p>
           Already have and account?{' '}
