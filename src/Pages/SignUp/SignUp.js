1786c0e8f0d65684667fd77907cb0c751267c64a
al-shaimon <alshaimon968@gmail.com>
2022-11-16 19:17:33 +0600

    73-5 Use RegEx validation in React Hook form

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -3,11 +3,15 @@ import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
 const SignUp = () => {
-  const { register, handleSubmit, formState: {errors} } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const handleSignUp = (data) => {
     console.log(data);
-  }
+  };
 
   return (
     <div className="h-[800px] flex justify-center items-center">
@@ -21,11 +25,11 @@ const SignUp = () => {
             <input
               type="text"
               {...register('name', {
-                required: 'Name is required'
+                required: 'Name is required',
               })}
               className="input input-bordered w-full max-w-xs"
             />
-            {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+            {errors.name && <p className="text-red-500">{errors.name.message}</p>}
           </div>
           <div className="form-control w-full max-w-xs">
             <label className="label">
@@ -34,11 +38,11 @@ const SignUp = () => {
             <input
               type="email"
               {...register('email', {
-                required: 'Email is required'
+                required: 'Email is required',
               })}
               className="input input-bordered w-full max-w-xs"
             />
-            {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
+            {errors.email && <p className="text-red-500">{errors.email.message}</p>}
           </div>
           <div className="form-control w-full max-w-xs">
             <label className="label">
@@ -48,11 +52,15 @@ const SignUp = () => {
               type="password"
               {...register('password', {
                 required: 'Password is required',
-                minLength: {value: 6, message: 'Password must be 6 characters long'}
+                minLength: { value: 6, message: 'Password must be 6 characters long' },
+                pattern: {
+                  value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/,
+                  message: 'Password must be strong',
+                },
               })}
               className="input input-bordered w-full max-w-xs"
             />
-            {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
+            {errors.password && <p className="text-red-500">{errors.password.message}</p>}
           </div>
           <input className="btn btn-accent w-full" value="Sign Up" type="submit" />
         </form>
