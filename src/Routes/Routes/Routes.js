c03ee01abacdfda76f82b094bedcbdae0e6d8ae9
al-shaimon <alshaimon968@gmail.com>
2022-11-20 01:50:43 +0600

    75-7 Load all users on the Dashboard page

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import DashboardLayout from '../../Layout/DashboardLayout';
 import Main from '../../Layout/Main';
 import Appointment from '../../Pages/Appointment/Appointment/Appointment';
+import AllUsers from '../../Pages/Dashboard/AllUsers/AllUsers';
 import Dashboard from '../../Pages/Dashboard/Dashboard/Dashboard';
 import MyAppointment from '../../Pages/Dashboard/MyAppointment/MyAppointment';
 import Home from '../../Pages/Home/Home/Home';
@@ -42,9 +43,13 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/dashboard',
-        element: <MyAppointment></MyAppointment>
-      }
-    ]
+        element: <MyAppointment></MyAppointment>,
+      },
+      {
+        path: '/dashboard/allusers',
+        element: <AllUsers></AllUsers>,
+      },
+    ],
   },
 ]);
 
