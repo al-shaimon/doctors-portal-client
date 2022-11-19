185cff2c9401d2a136ad6d91345141fdc6cc1b4d
al-shaimon <alshaimon968@gmail.com>
2022-11-20 02:38:04 +0600

    75-9 (advanced) Prevent accessing Admin route via URL

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { AuthContext } from '../contexts/AuthProvider';
+import useAdmin from '../hooks/useAdmin';
 import Navbar from '../Pages/Shared/Navbar/Navbar';
 
 const DashboardLayout = () => {
+  const { user } = useContext(AuthContext);
+  const [isAdmin] = useAdmin(user?.email);
   return (
     <div>
       <Navbar></Navbar>
@@ -19,7 +23,11 @@ const DashboardLayout = () => {
               <Link to="/dashboard">My Appointments</Link>
             </li>
             <li>
-              <Link to="/dashboard/allusers">All Users</Link>
+              {isAdmin && (
+                <>
+                  <Link to="/dashboard/allusers">All Users</Link>
+                </>
+              )}
             </li>
           </ul>
         </div>
