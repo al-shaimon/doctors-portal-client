544093d3fcf7c1a64ecb51f02243489ba3492e7a
al-shaimon <alshaimon968@gmail.com>
2022-11-21 05:29:13 +0600

    77-7 Process card confirmation for Payment

--- src/Pages/Dashboard/Payment/CheckoutForm.js
+++ src/Pages/Dashboard/Payment/CheckoutForm.js
@@ -1,12 +1,35 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-const CheckoutForm = ({booking}) => {
+const CheckoutForm = ({ booking }) => {
   const [cardError, setCardError] = useState('');
+  const [clientSecret, setClientSecret] = useState('');
   const stripe = useStripe();
   const elements = useElements();
 
-  const {price} = booking;
+  const { price, email, patient } = booking;
+
+  useEffect(() => {
+    // Create PaymentIntent as soon as the page loads
+    fetch('http://localhost:5000/create-payment-intent', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `bearer ${localStorage.getItem('accessToken')}`,
+      },
+      body: JSON.stringify({ price }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, [price]);
+
+  const appearance = {
+    theme: 'stripe',
+  };
+  const options = {
+    clientSecret,
+    appearance,
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -29,6 +52,16 @@ const CheckoutForm = ({booking}) => {
     } else {
       setCardError('');
     }
+
+    const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(clientSecret, {
+      payment_method: {
+        card: card,
+        billing_details: {
+          name: patient,
+          email: email,
+        },
+      },
+    });
   };
 
   return (
@@ -50,7 +83,11 @@ const CheckoutForm = ({booking}) => {
             },
           }}
         />
-        <button className="btn btn-sm mt-4 btn-primary" type="submit" disabled={!stripe}>
+        <button
+          className="btn btn-sm mt-4 btn-primary"
+          type="submit"
+          disabled={!stripe || !clientSecret}
+        >
           Pay
         </button>
       </form>
