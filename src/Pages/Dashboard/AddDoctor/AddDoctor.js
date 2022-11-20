e729133827f1ecb7fbc59f2719cd6cbfe288f92a
al-shaimon <alshaimon968@gmail.com>
2022-11-20 19:15:14 +0600

    76-3 File Type image field and understand form data

--- src/Pages/Dashboard/AddDoctor/AddDoctor.js
+++ src/Pages/Dashboard/AddDoctor/AddDoctor.js
@@ -1,5 +1,7 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import Loading from '../../Shared/Loading/Loading';
 
 const AddDoctor = () => {
   const {
@@ -8,9 +10,22 @@ const AddDoctor = () => {
     formState: { errors },
   } = useForm();
 
+  const { data: specialties, isLoading } = useQuery({
+    queryKey: ['specialty'],
+    queryFn: async () => {
+      const res = await fetch('http://localhost:5000/appointmentSpecialty');
+      const data = await res.json();
+      return data;
+    },
+  });
+
   const handleAddDoctor = (data) => {
     console.log(data);
   };
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
 
   return (
     <div className="w-96 p-7">
@@ -22,6 +37,7 @@ const AddDoctor = () => {
           </label>
           <input
             type="text"
+            placeholder="Enter Your Name"
             {...register('name', {
               required: 'Name is required',
             })}
@@ -35,6 +51,7 @@ const AddDoctor = () => {
           </label>
           <input
             type="email"
+            placeholder="Enter Your Email"
             {...register('email', {
               required: 'Email is required',
             })}
@@ -46,13 +63,28 @@ const AddDoctor = () => {
           <label className="label">
             <span className="label-text">Specialty</span>
           </label>
-          <select className="select select-bordered w-full max-w-xs">
-            <option disabled selected>
-              Pick your Specialty
-            </option>
-            <option>Han Solo</option>
-            <option>Greedo</option>
+          <select 
+          {...register('specialty')}
+          className="select input-bordered w-full max-w-xs">
+            {specialties.map((specialty) => (
+              <option key={specialty._id} value={specialty.name}>
+                {specialty.name}
+              </option>
+            ))}
           </select>
+        </div>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Photo</span>
+          </label>
+          <input
+            type="file"
+            {...register('img', {
+              required: 'Photo is required',
+            })}
+            className="input input-bordered w-full max-w-xs"
+          />
+          {errors.img && <p className="text-red-500">{errors.img.message}</p>}
         </div>
         <input className="btn btn-accent w-full mt-4" value="Add Doctor" type="submit" />
       </form>
