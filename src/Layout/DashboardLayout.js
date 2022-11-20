b1102cdb93fefc9d5a9aebaab39274206b11c88c
al-shaimon <alshaimon968@gmail.com>
2022-11-20 17:30:25 +0600

    76-1 Add a doctor admin route setup

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -18,17 +18,20 @@ const DashboardLayout = () => {
         <div className="drawer-side">
           <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
           <ul className="menu p-4 w-80 bg-base-100 text-base-content">
-            {/* <!-- Sidebar content here --> */}
             <li>
               <Link to="/dashboard">My Appointments</Link>
             </li>
-            <li>
-              {isAdmin && (
-                <>
+
+            {isAdmin && (
+              <>
+                <li>
                   <Link to="/dashboard/allusers">All Users</Link>
-                </>
-              )}
-            </li>
+                </li>
+                <li>
+                  <Link to="/dashboard/adddoctor">Add A Doctor</Link>
+                </li>
+              </>
+            )}
           </ul>
         </div>
       </div>
